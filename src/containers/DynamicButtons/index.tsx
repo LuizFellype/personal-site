3cf58afbb6ba7efa4dbc946690a19474832dcd4b
Luiz Fellype Cassago <lfcassago@gmail.com>
2023-12-18 19:01:31 -0300

    fix: space after config buttons

--- src/containers/DynamicButtons/index.tsx
+++ src/containers/DynamicButtons/index.tsx
@@ -6,7 +6,7 @@ type DynamicButtonsPropsType = { buttons: DynamicButtonsType[], absolute?: boole
 
 function NavigationHeader({ buttons, absolute = true }: DynamicButtonsPropsType) {
     return (
-        <div className='flex justify-between w-full'>
+        <div className='flex justify-between w-full mb-1'>
             {buttons.map(({ label, ...buttonProps }) => <button key={label}
                 className={`text-orange-400 border-dotted border-orange-400 border-2 p-1 rounded-md ${absolute && 'absolute'} ${buttonProps.disabled && 'text-gray-400 border-gray-400'}`}
                 {...buttonProps}
