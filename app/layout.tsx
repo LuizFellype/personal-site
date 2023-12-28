8ecc9468c548cc9913b531b6115db05c1b48ff43
Luiz Fellype Cassago <lfcassago@gmail.com>
2023-12-28 18:44:18 -0300

    feat: another approach for offiline to work

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,53 @@ import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = 'BaskSanta'
+const APP_DEFAULT_TITLE = "Basket Stats";
+const APP_TITLE_TEMPLATE = "%s - Bask Santa";
+const APP_DESCRIPTION = 'App to score basket stats 3x3 match.';
+
 export const metadata: Metadata = {
-  title: 'Basket Stats',
-  description: 'App to score basket stats 3x3 match',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
+
+ 
+export const viewport = {
+  themeColor: '#d3acfe',
+}
+ 
+
 
 export default function RootLayout({
   children,
--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-	dest: 'public',
-	register: true,
-	skipWaiting: true,
-})
+// const withPWA = require('next-pwa')({
+// 	dest: 'public',
+// 	register: true,
+// 	skipWaiting: true,
+// })
 
-module.exports = withPWA({
-	reactStrictMode: true,
-})
+// module.exports = withPWA({
+// 	reactStrictMode: true,
+// })
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+	dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	// disable: process.env.NODE_ENV === "development",
+	workboxOptions: {
+	  disableDevLogs: true,
+	},
+	// ... other options you like
+  });
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+	// ... other options you like
+  };
+  
+  module.exports = withPWA(nextConfig);
